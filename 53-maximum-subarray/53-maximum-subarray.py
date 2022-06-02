3a39c440b30d7823db653c7f529481fb335a5a16
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-06-02 22:50:40 +0900

    Time: 1902 ms (5.01%), Space: 28.4 MB (15.66%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,5 +1,15 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        start = nums[0]
+        answer = nums[0]
+        for i in range(1, len(nums)):
+            start = max(start+nums[i], nums[i])
+            answer = max(answer, start)
+        return answer 
+    
+'''
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
         return self.__div_conquer(nums, 0, len(nums)-1)
     
     def __div_conquer(self, nums, left, right):
@@ -19,3 +29,4 @@ class Solution:
         div_right = self.__div_conquer(nums, mid+1, right)
         
         return max(div_left, div_right, now)
+'''
