50aaa92e5596ed8d63dbab37b4bb80728cfade2e
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-06-07 22:31:04 +0900

    Time: 30 ms (89.62%), Space: 13.9 MB (11.30%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,6 +1,7 @@
 class Solution:
+    dic = {1:1, 2:2}
     def climbStairs(self, n: int) -> int:
-        a, b = 1, 1
-        for i in range(1, n):
-            a, b = b, a+b
-        return b
+        if n in self.dic.keys():
+            return self.dic[n]
+        self.dic[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.dic[n]
