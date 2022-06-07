92e76e569c759abdd54bbfa7c6d0d3aa8b4136a4
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-06-07 22:03:32 +0900

    Time: 132 ms (49.86%), Space: 20.6 MB (97.53%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -2,8 +2,8 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         answer = [0]
         for i in range(1, n+1):
-            if i%2 == 0:
-                answer.append(answer[i//2])
-            else:
+            if i%2:
                 answer.append(answer[i-1]+1)
+            else:
+                answer.append(answer[i//2])
         return answer
