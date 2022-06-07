3cbb53b0141997d8ddc33bc669d0ec55c8e2fb11
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-06-07 22:02:23 +0900

    Time: 181 ms (26.84%), Space: 20.8 MB (28.46%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,14 +1,9 @@
-import math
-
 class Solution:
     def countBits(self, n: int) -> List[int]:
         answer = [0]
         for i in range(1, n+1):
-            cnt = 0
-            leftN = logN = i
-            while leftN > 0:
-                logN = math.trunc(math.log2(leftN))
-                leftN -= 2**logN
-                cnt += 1
-            answer.append(cnt)
+            if i%2 == 0:
+                answer.append(answer[i//2])
+            else:
+                answer.append(answer[i-1]+1)
         return answer
