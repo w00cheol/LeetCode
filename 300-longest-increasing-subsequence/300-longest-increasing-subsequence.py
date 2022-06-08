dc928d38ef4cda99605ce5373feefd37a48d09e8
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-06-09 00:16:44 +0900

    Time: 120 ms (78.63%), Space: 14.2 MB (46.67%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,9 +1,11 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         dp = [nums[0]]
+        
         for i in range(1, len(nums)):
             if nums[i] > dp[-1]:
                 dp.append(nums[i])
+                
             elif nums[i] < dp[-1]:
                 mid, l, r, temp = 0, 0, len(dp)-1, len(dp)-1
                 while l <= r:
@@ -17,4 +19,5 @@ class Solution:
                     else:
                         l = mid + 1
                 dp[temp] = nums[i]
+                
         return len(dp)
