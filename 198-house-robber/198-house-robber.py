b989127c04519274b573815c5826f93deec2c552
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-06-14 19:54:22 +0900

    Time: 44 ms (52.28%), Space: 13.8 MB (64.74%) - LeetHub

---
+++ 198-house-robber/198-house-robber.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) > 1:
+            nums[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            nums[i] = max(nums[i-1], nums[i-2] + nums[i])
+        return nums[-1]
