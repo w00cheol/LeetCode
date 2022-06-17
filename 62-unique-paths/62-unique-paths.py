11f0b0af9a25f237e64acb83c1211830b2abfc6c
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-06-17 21:06:42 +0900

    Time: 56 ms (24.88%), Space: 13.9 MB (73.22%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.py
@@ -0,0 +1,11 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        answer = []
+        grid = [[1 for _ in range(m)] for _ in range(n)]
+        for i in range(m*n):
+            x = i // m
+            y = i % m
+            if x and y:
+                grid[x][y] = grid[x-1][y] + grid[x][y-1]
+        
+        return grid[-1][-1]
