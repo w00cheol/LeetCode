9c77155b9a46f81251c09e6dc969bd2523f0e099
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-06-20 22:04:53 +0900

    Time: 239 ms (34.16%), Space: 18 MB (98.62%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -7,7 +7,6 @@ class Solution:
                 if intervals[i][1] < answer[-1][0]:
                     answer.append(intervals[i])
                 else:
-                    answer[-1][0] = min(answer[-1][0], intervals[i][0])
                     answer[-1][1] = max(answer[-1][1], intervals[i][1])
             else:
                 answer.append(intervals[i])
