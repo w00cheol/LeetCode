aa70aa28986a510f9f8fb3691fbfd1f78298f3cc
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-06-25 16:15:16 +0900

    Time: 68 ms (11.62%), Space: 13.9 MB (68.84%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -3,7 +3,7 @@ class Solution:
         here, interval_here = head, head
         for _ in range(n):
             interval_here = interval_here.next
-        if not interval_here:
+        if interval_here is None:
             return head.next
         while interval_here.next:
             here, interval_here = here.next, interval_here.next
