84fc7d5cda40b14becf3b8b996e9920779421328
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-06-27 20:04:11 +0900

    Time: 268 ms (7.88%), Space: 34.5 MB (5.47%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,22 @@
+class Solution:
+    def dfs(self, i, dic, visited):
+        if i in visited:
+            return False
+        visited.append(i)
+        for j in dic[i]:
+            if self.dfs(j, dic, visited[::]) == False:
+                return False
+        dic[i] = []
+        return True
+                
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        dic = {}
+        for i in range(numCourses):
+            dic[i] = []
+        for i in range(len(prerequisites)):
+            dic[prerequisites[i][0]].append(prerequisites[i][1])
+        for i in range(numCourses):
+            visited = []
+            if self.dfs(i, dic, visited) == False:
+                return False
+        return True
