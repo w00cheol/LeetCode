6d257dcc3d00ad5f2a7bef740ed39884c2443979
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-06-29 21:38:05 +0900

    Time: 757 ms (43.79%), Space: 31.5 MB (13.57%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -9,12 +9,12 @@ class Solution:
                 cnt = 1
                 left_index, right_index = keys[i]-1, keys[i]+1
                 while dic.get(left_index, False):
+                    cnt += 1
                     dic[left_index] = False
                     left_index -= 1
-                    cnt += 1
                 while dic.get(right_index, False):
+                    cnt += 1
                     dic[right_index] = False
                     right_index += 1
-                    cnt += 1
                 answer = max(answer, cnt)
         return answer
