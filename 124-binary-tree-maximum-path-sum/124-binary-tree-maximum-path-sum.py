19436dc57055b81c77ae37f2d1d4e6c1f7ae35a2
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-07-07 21:29:32 +0900

    Time: 86 ms (96.41%), Space: 21.3 MB (94.35%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -1,7 +1,8 @@
 class Solution:
     memorize = -2e9
     def includeRoot(self, root):
-        if not root: return -2e9
+        if root is None:
+            return -2e9
         M_left, M_right = self.includeRoot(root.left), self.includeRoot(root.right)
         include_root = max(root.val, root.val + max(M_left, M_right))
         self.memorize = max(self.memorize, include_root, M_left + root.val + M_right)
