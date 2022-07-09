e20fa44948d6286a7cf774ee4674a293c0944afc
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-07-09 23:00:31 +1000

    Time: 563 ms (5.41%), Space: 23.9 MB (9.65%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.py
@@ -1,7 +1,7 @@
 class Codec:
     def dfs(self, data):
-        if data[:2] == 'N+': return data[2:], None
-        index = data.find('+')
+        if data[0] == 'N': return data[2:], None
+        index = data.find('!')
         root = TreeNode(int(data[:index]))
         data = data[index+1:]
         data, root.left = self.dfs(data)
@@ -10,10 +10,10 @@ class Codec:
     
     def serialize(self, root, answer_str = ''):
         if not root:
-            answer_str += 'N+'
+            answer_str += 'N!'
         else:
             answer_str += str(root.val)
-            answer_str += '+'
+            answer_str += '!'
             answer_str = self.serialize(root.left, answer_str)
             answer_str = self.serialize(root.right, answer_str)
         return answer_str
