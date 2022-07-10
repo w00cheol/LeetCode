b9ada8ad5fc2cd611dbc19d50563c226d38fa299
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-07-10 13:41:13 +1000

    Time: 41 ms (81.90%), Space: 15 MB (8.83%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -1,11 +1,12 @@
 from collections import defaultdict
 class Solution:
-    def dfs(self, root, depth, answer_dict):
-        if not root: return answer_dict
-        answer_dict[depth].append(root.val)
-        answer_dict = self.dfs(root.left, depth + 1, answer_dict)
-        answer_dict = self.dfs(root.right, depth + 1, answer_dict)
-        return answer_dict
-    
+    def dfs(self, root, depth, answer):
+        if not root: return answer
+        answer[depth].append(root.val)
+        self.dfs(root.left, depth + 1, answer)
+        self.dfs(root.right, depth + 1, answer)
+        
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        return self.dfs(root, 0, defaultdict(list)).values()
+        answer = defaultdict(list)
+        self.dfs(root, 0, answer)
+        return answer.values()
