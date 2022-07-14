efcb04ac0bc1d0f7f09b41ba3061bc6844a8863e
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-07-14 23:34:29 +1000

    Time: 103 ms (16.43%), Space: 18 MB (44.27%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -1,6 +1,6 @@
 class Solution:
     def dfs(self, root, answer):
-        if not root: return None
+        if not root: return
         answer.append(root.val)
         self.dfs(root.left, answer)
         self.dfs(root.right, answer)
