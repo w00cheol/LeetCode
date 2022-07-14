013b6b9db4fa6c72b67dfac27366956f5786a9d6
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-07-14 23:33:18 +1000

    Time: 91 ms (31.81%), Space: 18 MB (88.55%) - LeetHub

---
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -0,0 +1,10 @@
+class Solution:
+    def dfs(self, root, answer):
+        if root == None: return
+        answer.append(root.val)
+        self.dfs(root.left, answer)
+        self.dfs(root.right, answer)
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        answer = []
+        self.dfs(root, answer)
+        return sorted(answer)[k-1]
