03077509ab1c2412ef63e27c559e3ed66027eeca
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-07-14 23:35:37 +1000

    Time: 57 ms (87.64%), Space: 18 MB (88.55%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -1,9 +1,9 @@
 class Solution:
     def dfs(self, root, answer):
         if root == None: return None
-        answer.append(root.val)
         self.dfs(root.left, answer)
         self.dfs(root.right, answer)
+        answer.append(root.val)
         
     def kthSmallest(self, root: Optional[TreeNode], k: int):
         answer = []
