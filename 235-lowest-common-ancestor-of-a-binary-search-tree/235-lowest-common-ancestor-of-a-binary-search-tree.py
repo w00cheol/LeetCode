9a97ef26ac5d441d329efd95b06100d9252668ac
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-07-15 21:19:23 +1000

    Time: 116 ms (55.63%), Space: 18.7 MB (68.08%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,8 +1,13 @@
 class Solution:
     def greedy(self, root, p, q):
-        if root.val == p or root.val == q: return root
-        if p < root.val < q: return root
-        if root.val < p: return self.greedy(root.right, p, q)
-        if q < root.val: return self.greedy(root.left, p, q)
+        if root.val == p or root.val == q:
+            return root
+        if p < root.val < q:
+            return root
+        if root.val < p:
+            return self.greedy(root.right, p, q)
+        if q < root.val:
+            return self.greedy(root.left, p, q)
+        
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         return self.greedy(root, min(p.val, q.val), max(p.val, q.val))
