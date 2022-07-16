c2cd5defd2ae7f0ef4cacff0324312542c73b2ae
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-07-17 00:21:42 +1000

    Time: 1604 ms (5.01%), Space: 21.2 MB (95.33%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -3,9 +3,9 @@ class Trie:
     def __init__(self):
         self.answer = set()
         
-    def insert(self, word: str) -> None:
+    def insert(self, word: str):
         self.answer.add(word)
-    def search(self, word: str) -> bool:
+    def search(self, word: str):
         return word in self.answer
-    def startsWith(self, prefix: str) -> bool:
+    def startsWith(self, prefix: str):
         return any(map(lambda x: x.startswith(prefix), self.answer))
