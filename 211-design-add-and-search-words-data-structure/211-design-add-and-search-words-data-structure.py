aca68a02d6711adbbc7ab7974168a0ad4b092b07
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-07-17 23:41:16 +1000

    Time: 19092 ms (6.63%), Space: 55.8 MB (86.55%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,24 @@
+class WordDictionary:
+    def __init__(self):
+        self.dic = {}
+
+    def addWord(self, word: str) -> None:
+        d = self.dic
+        for w in word:
+            if w not in d:
+                d[w] = {}
+            d = d[w]
+        d['end'] = True
+
+    def dfs(self, d, word):
+        if not word:
+            if 'end' in d:
+                return True
+            return False
+        for key in d.keys():
+            if key != 'end' and word[0] == '.' or word[0] == key:
+                if self.dfs(d[key], word[1:]): return True
+        return False
+    
+    def search(self, word: str, d = 0) -> bool:
+        return self.dfs(self.dic, word)
