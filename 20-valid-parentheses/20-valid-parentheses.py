d58d311dcd0bf05815895712f367c690e24337c7
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-07-19 00:55:30 +1000

    Time: 37 ms (81.53%), Space: 13.9 MB (70.03%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -5,7 +5,6 @@ class Solution:
         for i in range(len(s)):
             if s[i] in dic:
                 answer.append(dic[s[i]])
-            else:
-                if len(answer) == 0 or s[i] != answer.pop(): return False
+            elif len(answer) == 0 or s[i] != answer.pop(): return False
         return len(answer) == 0
         
