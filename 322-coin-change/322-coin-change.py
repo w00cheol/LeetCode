2f6d1589ed6b8d7c1dca19b6c020b8e2c1392a08
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-07-20 06:45:51 +1000

    Time: 2417 ms (37.26%), Space: 14.4 MB (37.94%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -3,6 +3,5 @@ class Solution:
         answer = [0] + [2e9 for _ in range(amount)]
         for i in range(1, amount + 1):
             for coin in coins:
-                if i - coin >= 0 and answer[i - coin] != 2e9:
-                    answer[i] = min(answer[i], 1 + answer[i - coin])
+                if i - coin >= 0 and answer[i - coin] != 2e9: answer[i] = min(answer[i], 1 + answer[i - coin])
         return answer[-1] if answer[-1] != 2e9 else -1
