5e5288e35dd4ad5771dae81b352bfc6b8b8afd60
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-07-22 23:35:01 +1000

    Time: 163 ms (72.24%), Space: 14.7 MB (52.85%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -1,13 +1,27 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        row, col = set(), set()
+        dic = {'row': False, 'col': False}
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
-                    row.add(i)
-                    col.add(j)
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if i in row or j in col:
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+                    if i == 0: dic['row'] = True
+                    if j == 0: dic['col'] = True
+        for i in range(1, len(matrix[0])):
+            if matrix[0][i] == 0:
+                for j in range(1, len(matrix)):
+                    if matrix[j][i] != 0: matrix[j][i] = '#'
+        for i in range(1, len(matrix)):
+            if matrix[i][0] == 0:
+                for j in range(1, len(matrix[0])):
                     matrix[i][j] = 0
-        return matrix
+                continue
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == '#': matrix[i][j] = 0
+        if dic['row']:
+            for i in range(len(matrix[0])):
+                matrix[0][i] = 0
+        if dic['col']:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
