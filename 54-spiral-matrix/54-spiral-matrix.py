5c97f2545f66396ae085d678876b850e474e1447
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-07-23 23:32:01 +1000

    Time: 30 ms (95.12%), Space: 14 MB (32.78%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -1,9 +1,9 @@
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+    def spiralOrder(self, matrix):
         x, y = 0, 0
         answer = [matrix[x][y]]
         matrix[x][y] = None
-        while len(answer) < len(matrix)*len(matrix[0]):
+        while len(answer) != len(matrix)*len(matrix[0]):
             while y+1 < len(matrix[0]) and matrix[x][y+1] != None:
                     y += 1
                     answer.append(matrix[x][y])
