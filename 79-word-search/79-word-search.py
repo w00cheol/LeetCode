d85e4b7358cb538838413a2b9b055a634e076674
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-07-28 01:12:44 +1000

    Time: 8542 ms (29.29%), Space: 13.9 MB (92.85%) - LeetHub

---
+++ 79-word-search/79-word-search.py
@@ -0,0 +1,23 @@
+class Solution:
+    def exist(self, board, word):
+        def find(board, i, j, word):
+            if board[i][j] == word[0]:
+                if len(word) == 1:
+                    return True
+                value = board[i][j]
+                board[i][j] = None
+                if i+1 < len(board):
+                    if find(board, i+1, j, word[1:]): return True
+                if j+1 < len(board[0]):
+                    if find(board, i, j+1, word[1:]): return True
+                if i-1 >= 0:
+                    if find(board, i-1, j, word[1:]): return True
+                if j-1 >= 0:
+                    if find(board, i, j-1, word[1:]): return True
+                board[i][j] = value
+            return False
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if find(board, i, j, word): return True
+        return False
