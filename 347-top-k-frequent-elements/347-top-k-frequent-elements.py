c0787c994d9204cbcbf040e7044a451b766cf890
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-07-30 16:33:34 +1000

    Time: 158 ms (56.66%), Space: 18.7 MB (47.46%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def topKFrequent(self, nums, k):
         dic = defaultdict(int)
         for num in nums:
             dic[num] += 1
