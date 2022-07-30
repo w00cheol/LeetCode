e2308c7adbcc0e95d890ae028c3c249d1fae9a26
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-07-30 16:33:02 +1000

    Time: 141 ms (68.93%), Space: 18.8 MB (47.46%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic = defaultdict(int)
+        for num in nums:
+            dic[num] += 1
+        srtd = sorted(zip(dic.keys(), dic.values()), key=lambda x: x[1])
+        answer = []
+        for _ in range(k):
+            answer.append(srtd.pop()[0])
+        return answer
