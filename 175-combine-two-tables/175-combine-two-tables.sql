3ab05ed9eff7ed942846b905e6778b4e69e51b54
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-08-02 00:35:45 +1000

    Time: 393 ms (71.10%), Space: 0B (100.00%) - LeetHub

---
+++ 175-combine-two-tables/175-combine-two-tables.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT
+    firstName,
+    lastName,
+    city,
+    state
+FROM
+    Person as p
+LEFT JOIN
+    Address as a
+ON
+    p.personId = a.personId
