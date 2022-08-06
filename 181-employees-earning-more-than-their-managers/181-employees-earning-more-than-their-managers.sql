f8d278a56d26449f53af9e2b3bb6928361b8fef0
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-08-07 00:48:29 +1000

    Time: 910 ms (15.96%), Space: 0B (100.00%) - LeetHub

---
+++ 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
@@ -0,0 +1,12 @@
+SELECT
+    name as Employee
+FROM
+    Employee as E
+WHERE
+    E.salary > (SELECT
+                    M.salary
+                FROM
+                    Employee as M
+                WHERE
+                    E.managerid = M.id
+               )
