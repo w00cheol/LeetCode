c31f7c1dcd723af2f668809ea394d72c6d4422e7
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-08-19 20:00:14 +0900

    Time: 67 ms (7.27%), Space: 14 MB (13.21%) - LeetHub

---
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -0,0 +1,11 @@
+class Solution:
+    answer = []
+    def inorder(self, root):
+        if root is None: return
+        self.inorder(root.left)
+        self.answer.append(root.val)
+        self.inorder(root.right)
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        self.answer = []
+        self.inorder(root)
+        return self.answer
