b6900c71f64ee7f93102c3af4259d72f903b6ea7
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-08-20 19:46:36 +0900

    Time: 9276 ms (16.13%), Space: 14.1 MB (93.39%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -1,5 +1,5 @@
 class Solution:
-    def threeSumClosest(self, nums: List[int], target: int) -> int:
+    def threeSumClosest(self, nums, target):
         nums.sort()
         answer = nums[0] + nums[1] + nums[2]
         for left in range(len(nums)-2):
