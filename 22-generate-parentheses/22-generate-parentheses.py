01f2d5717dfae5804cc5e0b00433635f581ba555
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-08-23 23:42:16 +0900

    Time: 44 ms (77.84%), Space: 14.1 MB (77.53%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generateParenthesis(self, n):
+        def dfs(string, popen, pclose, answer):
+            if pclose == 0:
+                answer.append(string)
+            
+            if popen > 0:
+                dfs(string + '(', popen-1, pclose, answer)
+                
+            if pclose > popen:
+                dfs(string + ')', popen, pclose-1, answer)
+                
+            return answer
+        
+        return dfs('', n, n, [])
