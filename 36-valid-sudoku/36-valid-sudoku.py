131444b4014f2531095ee9562ae66ca47583616b
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-08-25 23:30:22 +0900

    Time: 152 ms (50.55%), Space: 13.9 MB (35.29%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == '.':
+                    continue
+                
+                if board[i][j] not in rows[i]:
+                    rows[i].add(board[i][j])
+                else:
+                    return False
+                
+                if board[i][j] not in cols[j]:
+                    cols[j].add(board[i][j])
+                else:
+                    return False
+                
+                b = (i//3) * 3 + (j//3)
+                if board[i][j] not in boxes[b]:
+                    boxes[b].add(board[i][j])
+                else:
+                    return False
+        return True
