aa2d52dcf976425c7429fb54af289d071612ec10
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-08-26 21:59:19 +0900

    Time: 58 ms (19.76%), Space: 14 MB (39.83%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.py
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -6,10 +6,8 @@ class Solution:
         while s[index] == ' ':
             index -= 1
         
-        while s[index] != ' ':
+        while index >= 0 and s[index] != ' ':
             answer += 1
             index -= 1
-            if index < 0:
-                return answer
             
         return answer
