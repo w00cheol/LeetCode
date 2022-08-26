31ad31007ab4ee0a94284189d39413799a21cc71
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-08-26 21:57:44 +0900

    Time: 53 ms (32.49%), Space: 13.8 MB (80.81%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        answer = 0
+        index = len(s) - 1
+        
+        while s[index] == ' ':
+            index -= 1
+        
+        while s[index] != ' ':
+            answer += 1
+            index -= 1
+            if index < 0:
+                break
+            
+        return answer
