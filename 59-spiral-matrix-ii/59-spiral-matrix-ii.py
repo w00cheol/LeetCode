0ef7442beabfc4e242f1c90c56cf2defd8d0a19f
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-10-08 22:15:56 +0900

    Time: 62 ms (32.73%), Space: 13.8 MB (82.63%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        answer = [[0] * n for _ in range(n)]
+        x, y = 0, 0
+        dx, dy = 1, 0
+        
+        for i in range(1, n*n + 1):
+            answer[y][x] = i
+            
+            if x+dx == n or y+dy == n or answer[y+dy][x+dx]:
+                dx, dy = -dy, dx
+                
+            x = x + dx
+            y = y + dy
+    
+        return answer
+    
+    
