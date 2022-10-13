a2cb8032b364ab64f1881df4988f673bd67d6183
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-10-13 16:30:38 +0900

    Time: 526 ms (55.09%), Space: 14.2 MB (58.35%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]):
+        answer = [1] + [0] * amount # the answer of index that indicates the number of amount
+        
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                answer[i] += answer[i - coin]
+                        
+        return answer[amount]
