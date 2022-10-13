ed9437251dccedddc80975d9b921b0b16564449a
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-10-13 15:21:53 +0900

    Time: 93 ms (6.74%), Space: 14 MB (31.99%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.py
@@ -0,0 +1,32 @@
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        new_head = head
+        runner = head
+        
+        node_nums = 1
+        for _ in range(k):
+            if runner.next is None:
+                runner = head
+                
+                for _ in range(k % node_nums):
+                    runner = runner.next
+                    
+                break
+            else:
+                runner = runner.next
+                node_nums += 1
+            
+            
+        while runner.next is not None:
+            runner = runner.next
+            new_head = new_head.next
+        
+        runner.next = head
+        
+        temp = new_head
+        new_head = new_head.next
+        temp.next = None
+        
+        return new_head
