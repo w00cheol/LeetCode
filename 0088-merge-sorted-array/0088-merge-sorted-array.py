debf7330a52d5c8798a3deafcb70ace03a95e4bf
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-10-14 22:22:20 +0900

    Time: 57 ms (69.74%), Space: 14 MB (37.99%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        while m and n:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[m + n - 1] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[m + n - 1] = nums2[n - 1]
+                n -= 1
+                
+        nums1[:n] = nums2[:n]
