02250c0911fc3a7e14afac4e47c54d3516a63a16
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-10-23 17:44:06 +0900

    Time: 73 ms (12.53%), Space: 14.1 MB (95.81%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -0,0 +1,31 @@
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
+        def divide_conquer(root, depth):
+            if root is None:
+                return {}
+            
+            small_answer = {}
+            small_answer[depth] = [root.val]
+                
+            l = divide_conquer(root.left, depth + 1)
+            r = divide_conquer(root.right, depth + 1)
+            
+            conquered_level = depth + 1
+            conquered_l = l.get(conquered_level, [])
+            conquered_r = r.get(conquered_level, [])
+            
+            while conquered_l != [] or conquered_r != []:
+                if conquered_level % 2 == 0:
+                    small_answer[conquered_level] = conquered_r + conquered_l
+                else:
+                    small_answer[conquered_level] = conquered_l + conquered_r
+                    
+                conquered_level += 1
+                conquered_l = l.get(conquered_level, [])
+                conquered_r = r.get(conquered_level, [])
+            
+            return small_answer
+        
+        answer = divide_conquer(root, 1)
+        return answer.values()
