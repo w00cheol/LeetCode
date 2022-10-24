20f10260819c145030ad3bb740f2a94df465171e
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-10-24 19:11:54 +0900

    Time: 1394 ms (5.00%), Space: 14.5 MB (46.77%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,39 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [['.' for _ in range(n)] for _ in range(n)]
+        answer = []
+        
+        def backTracking(board, row):
+            if isValid(board) == False:
+                return
+            if row == n:
+                answer.append([''.join(board[i]) for i in range(n)])
+                return
+            
+            for i in range(len(board[0])):
+                board[row][i] = 'Q'
+                backTracking(board, row + 1)
+                board[row][i] = '.'
+            
+            return
+        
+        def isValid(board):
+            l_upper_diag = set()
+            r_upper_diag = set()
+            
+            for c in range(len(board[0])):
+                col_has_queen = False
+                for r in range(len(board)):
+                    if board[r][c] == 'Q':
+                        if col_has_queen or (r-c) in l_upper_diag or (r+c) in r_upper_diag:
+                            return False
+                        else:
+                            col_has_queen = True
+                            l_upper_diag.add(r-c)
+                            r_upper_diag.add(r+c)
+            return True
+        
+        backTracking(board, 0)
+        return answer
+        
+        
