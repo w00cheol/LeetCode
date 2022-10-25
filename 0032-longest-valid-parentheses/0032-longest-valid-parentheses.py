e149f73a41f84777f33ada31c7a62cf25ef54e27
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-10-25 19:03:08 +0900

    Time: 85 ms (56.05%), Space: 14.2 MB (77.88%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -1,17 +1,22 @@
 class Solution:
-    def longestValidParentheses(self, s: str) -> int:
-        answer_set = [0]
-        max_answer = 0
+    def longestValidParentheses(self, s):
+        answer = 0
+        answer_group = [0]
         
         for i in range(len(s)):
-            if s[i] == '(':
-                answer_set.append(0)
-            else:
-                if len(answer_set) > 1:
-                    a = answer_set.pop()
-                    answer_set[-1] += a + 2
-                    max_answer = max(max_answer, answer_set[-1])
-                else:
-                    answer_set = [0]
+            if s[i] == '(': # beginning of new group
+                answer_group.append(0)
+            elif len(answer_group) > 1: # which means there is '(' that is still unclosed
+                # so we can remove one '(' to make one more group (length is 2)
+                last_group = answer_group.pop() + 2
                 
-        return max_answer
+                # if there is no '(' between two group we can combine them
+                answer_group[-1] += last_group
+                
+                # check new last group we have combine
+                answer = max(answer, answer_group[-1])
+            else:
+                # ')' exists more so we have to restart
+                answer_group = [0]
+                
+        return answer
