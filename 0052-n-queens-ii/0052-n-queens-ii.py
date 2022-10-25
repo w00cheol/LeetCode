ac9a24f49098920d3f7732fafa11bc378897da06
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-10-25 16:22:05 +0900

    Time: 91 ms (61.11%), Space: 14 MB (36.85%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        cols = set()
+        pos_diag = set()
+        neg_diag = set()
+        
+        def backTracking(row):
+            count = 0
+            if row == n:
+                return 1
+                
+            for col in range(n):
+                if col in cols or (row + col) in pos_diag or (row - col) in neg_diag:
+                    continue
+                
+                cols.add(col)
+                pos_diag.add(row + col)
+                neg_diag.add(row - col)
+                
+                count += backTracking(row+1)
+                
+                cols.remove(col)
+                pos_diag.remove(row + col)
+                neg_diag.remove(row - col)
+            
+            return count
+            
+        return backTracking(0)
