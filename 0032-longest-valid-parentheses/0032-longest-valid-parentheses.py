b464e6d6167b05d8830614f2d11e6711bac3a168
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-10-25 19:03:32 +0900

    Time: 42 ms (97.26%), Space: 14.2 MB (77.88%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -7,6 +7,7 @@ class Solution:
             if s[i] == '(': # beginning of new group
                 answer_group.append(0)
             elif len(answer_group) > 1: # which means there is '(' that is still unclosed
+                
                 # so we can remove one '(' to make one more group (length is 2)
                 last_group = answer_group.pop() + 2
                 
