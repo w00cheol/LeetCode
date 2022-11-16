7a8c6065833032287a65278ad540fe578e809c6d
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-11-17 00:07:38 +0900

    Time: 86 ms (52.38%), Space: 14.1 MB (44.36%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,30 +1,32 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid):
+        rows = len(obstacleGrid)
+        cols = len(obstacleGrid[0])
         if obstacleGrid[0][0] or obstacleGrid[-1][-1]:
             return 0
         
-        for row in range(len(obstacleGrid)):
-            for col in range(len(obstacleGrid[0])):
-                if obstacleGrid[row][col] == 1:
-                    obstacleGrid[row][col] = -1
+        for r in range(rows):
+            for c in range(cols):
+                if obstacleGrid[r][c] == 1:
+                    obstacleGrid[r][c] = -1
                     
         obstacleGrid[0][0] = 1
         
-        for row in range(1, len(obstacleGrid)):
-                if obstacleGrid[row][0] == 0 and obstacleGrid[row - 1][0] != -1:
-                    obstacleGrid[row][0] = 1
+        for r in range(1, rows):
+                if obstacleGrid[r][0] == 0 and obstacleGrid[r - 1][0] != -1:
+                    obstacleGrid[r][0] = 1
                 else:
-                    obstacleGrid[row][0] = -1
+                    obstacleGrid[r][0] = -1
                     
-        for col in range(1, len(obstacleGrid[0])):
-                if obstacleGrid[0][col] == 0 and obstacleGrid[0][col-1] != -1:
-                    obstacleGrid[0][col] = 1
+        for c in range(1, cols):
+                if obstacleGrid[0][c] == 0 and obstacleGrid[0][c-1] != -1:
+                    obstacleGrid[0][c] = 1
                 else:
-                    obstacleGrid[0][col] = -1
+                    obstacleGrid[0][c] = -1
 
-        for row in range(1, len(obstacleGrid)):
-            for col in range(1, len(obstacleGrid[0])):
-                if obstacleGrid[row][col] != -1:
-                    obstacleGrid[row][col] = max(0, obstacleGrid[row-1][col]) + max(0, obstacleGrid[row][col-1])
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if obstacleGrid[r][c] != -1:
+                    obstacleGrid[r][c] = max(0, obstacleGrid[r-1][c]) + max(0, obstacleGrid[r][c-1])
                     
         return obstacleGrid[-1][-1]
