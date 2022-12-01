6e6670604b6637c485f34912b69ff4025daee4d9
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-12-01 15:16:07 +0900

    Time: 61 ms (57.94%), Space: 13.7 MB (97.48%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        dic = {0: 0, 1:0, 2:0}
+        
+        for num in nums:
+            dic[num] += 1
+        
+        i = 0
+        for _ in range(dic[0]):
+            nums[i] = 0
+            i += 1
+        for _ in range(dic[1]):
+            nums[i] = 1
+            i += 1
+        for _ in range(dic[2]):
+            nums[i] = 2
+            i += 1
