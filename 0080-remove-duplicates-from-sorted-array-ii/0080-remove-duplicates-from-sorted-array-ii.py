74088622c33a52b390fb07b83db5d5e9d25d8b30
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-12-02 22:11:31 +0900

    Time: 62 ms (85.39%), Space: 13.9 MB (31.71%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -3,10 +3,8 @@ class Solution:
         runner = 2
         
         for i in range(2, len(nums)):
-            if nums[i] == nums[runner - 1] and nums[i] == nums[runner - 2]:
-                    continue
-                
-            nums[runner] = nums[i]
-            runner += 1
+            if nums[i] != nums[runner - 1] or nums[i] != nums[runner - 2]:
+                nums[runner] = nums[i]
+                runner += 1
             
         return min(len(nums), runner)
