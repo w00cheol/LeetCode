b57535ccb89f765295c80e447a21b12d6454fb33
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-12-06 00:09:27 +0900

    Time: 50 ms (67.76%), Space: 14 MB (86.72%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -1,30 +1,35 @@
 class Solution:
     def reverseBetween(self, head, left, right):
-        fake_head = ListNode(0)
+        # insert dummy node O(1)
+        fake_head = ListNode(None)
         fake_head.next = head
         
         curr = fake_head
         
+        # skip to (left - 1) O(N)
         for _ in range(left - 1):
             curr = curr.next
             
-        last_head = curr # idx : left - 1
+        last_head = curr # index = left - 1
         
-        curr = curr.next # idx += 1
-        last_body = curr # idx : left
+        curr = curr.next # index += 1
+        last_body = curr # index = left
         
-        # prev, curr = left, left + 1
+        # prev, curr index = left, left + 1
         prev = curr
         curr = curr.next
         
-        # reverse and slide window
+        # reverse two nodes O(n)
+        # shift each variable one index to right side
         for _ in range(right - left):
             next_node = curr.next
             curr.next = prev
             prev = curr
             curr = next_node
             
+        # combine three parts into one
         last_head.next = prev 
         last_body.next = curr
             
+        # return real head
         return fake_head.next
