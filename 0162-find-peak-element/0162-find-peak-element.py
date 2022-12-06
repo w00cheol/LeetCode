5df913d296f63b772ea6bf2919d19ea1cc3fc16c
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-12-06 16:57:23 +0900

    Time: 59 ms (79.10%), Space: 13.9 MB (98.73%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,12 +1,11 @@
 class Solution:
-    def findPeakElement(self, nums: List[int]) -> int:
+    def findPeakElement(self, nums) -> int:
         # we don't know haw many peaks in this array
         # but 100% SURE num keeps increasing at least one time (because the first num is INF !)
         # and it is supposed to be decrease at least one time (because the last num is -INF !)
         
         # so find the index and that will be an answer
-        l, r = 0, len(nums) - 1
-        mid = 0
+        l, r, mid = 0, len(nums) - 1, 0
         
         while l < r:
             mid = (l + r) // 2
