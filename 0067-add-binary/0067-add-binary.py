8f51e639a89977f76aa2ba3b18e087541bfbdad2
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-12-08 21:22:28 +0900

    Time: 48 ms (75.05%), Space: 14.1 MB (25.77%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,42 +1,27 @@
 class Solution:
     def addBinary(self, a, b):
-        i, j = len(a) - 1, len(b) - 1
-        cin = 0
         answer = ''
+        i, j, cin = len(a) - 1, len(b) - 1, 0
         
         while i >= 0 and j >= 0:
-            sum = int(a[i]) + int(b[j]) + cin
-            
-            if sum >= 2:
-                cin = 1
-                sum -= 2
-            else:
-                cin = 0
+            sum = int(a[i]) ^ int(b[j]) ^ cin
+            cin = (int(a[i]) & int(b[j])) | (int(a[i]) & cin) | (int(b[j]) & cin)
 
             i -= 1
             j -= 1
-            
             answer += str(sum)
             
         while i >= 0:
-            sum = int(a[i]) + cin
-            if sum == 2:
-                cin = 1
-                sum = 0
-            else:
-                cin = 0
-                
+            sum = int(a[i]) ^ cin
+            cin = int(a[i]) & cin
+            
             i -= 1
             answer += str(sum)
         
         while j >= 0:
-            sum = int(b[j]) + cin
-            if sum == 2:
-                cin = 1
-                sum = 0
-            else:
-                cin = 0
-                
+            sum = int(b[j]) ^ cin
+            cin = int(b[j]) & cin
+            
             j -= 1
             answer += str(sum)
             
