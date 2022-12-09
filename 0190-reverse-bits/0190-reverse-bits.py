3eea81c1729873421f41d53d29bc26a694c9c3be
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-12-09 16:28:55 +0900

    Time: 72 ms (18.48%), Space: 13.9 MB (50.28%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,9 +1,11 @@
 class Solution:
     def reverseBits(self, n):
-        answer = 0
+        answer = n & 1
         
-        for _ in range(32):
-            answer = (answer << 1) + (n & 1)
+        for _ in range(32 - 1):
+            answer <<= 1
             n >>= 1
             
+            answer |= (n & 1)
+        
         return answer
