7070daee6ccd237d12fe8ad8e5f05858a5e7b052
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-12-09 16:28:15 +0900

    Time: 78 ms (8.37%), Space: 13.9 MB (7.85%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -3,7 +3,7 @@ class Solution:
         answer = 0
         
         for _ in range(32):
-            answer = (answer << 1) | (n & 1)
+            answer = (answer << 1) + (n & 1)
             n >>= 1
             
         return answer
