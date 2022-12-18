5e2f0c6de01212d2e55bdb03776af2f11a73c81d
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-12-18 18:21:45 +0900

    Time: 60 ms (68.42%), Space: 14.2 MB (10.90%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        def dfs(idx, curr):
+            answers.append(curr)
+            
+            for i in range(idx, len(nums)):
+                if i == idx or nums[i] != nums[i- 1]:
+                    dfs(i + 1, curr + [nums[i]])
+                    
+        answers = []
+        nums.sort()
+        dfs(0, [])
+        
+        return answers
