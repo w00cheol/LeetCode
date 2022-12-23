24e46e0d8108dea7be1a650b6e9d87d8fff6f7ce
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2022-12-23 23:53:14 +0900

    Time: 105 ms (57.13%), Space: 14.4 MB (93.25%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def search(self, nums, target):
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            
+            if nums[mid] == target:
+                return True
+
+            elif nums[l] == nums[mid]:
+                if l != mid:
+                    l += 1
+                    continue
+                else:
+                    l = mid + 1
+
+            elif nums[l] < nums[mid]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+
+            elif nums[l] > nums[mid]:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+        return False
