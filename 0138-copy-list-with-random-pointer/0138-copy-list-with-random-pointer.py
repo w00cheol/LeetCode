16221a590746df2653d330d7f441fa45da3b0660
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2023-01-06 16:44:43 +0900

    Time: 81 ms (12.88%), Space: 14.8 MB (98.93%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -0,0 +1,19 @@
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        dic = {}
+        
+        runner = head
+        
+        while runner:
+            dic[runner] = Node(runner.val)
+            runner = runner.next
+            
+        runner = head
+        
+        while runner:
+            curr = dic.get(runner)
+            curr.next = dic.get(runner.next)
+            curr.random = dic.get(runner.random)
+            runner = runner.next
+            
+        return dic.get(head)
