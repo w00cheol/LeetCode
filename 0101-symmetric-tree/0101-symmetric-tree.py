111d007e1f997d804dc390125efdc2b8a24f763f
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2023-01-24 18:19:09 +0900

    Time: 46 ms (35.15%), Space: 13.9 MB (55.77%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node_l, node_r):
+            if node_l is None and node_r is None:
+                return True
+            
+            if node_l is None or node_r is None:
+                return False
+            
+            if node_l.val == node_r.val:
+                return dfs(node_l.right, node_r.left) and dfs(node_l.left, node_r.right)
+            else:
+                return False
+            
+        return dfs(root.left, root.right)
