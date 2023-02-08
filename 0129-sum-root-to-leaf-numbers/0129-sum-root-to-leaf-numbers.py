a0d36b94ffbb35e45cb81133d8990d7761da7297
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2023-02-08 15:28:35 +0900

    Time: 34 ms (64.04%), Space: 13.9 MB (15.08%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -0,0 +1,22 @@
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        global answer
+        answer = 0
+        
+        def dfs(running_sum, node):
+            running_sum += node.val
+            
+            if not node.left and not node.right:
+                global answer
+                answer += running_sum
+                return
+            
+            if node.left:
+                dfs(running_sum * 10, node.left)
+                
+            if node.right:
+                dfs(running_sum * 10, node.right)
+                
+        dfs(0, root)        
+                
+        return answer
