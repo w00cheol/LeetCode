ce54069dbac671577f955243eb56904729a802e8
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2023-02-18 17:33:11 +0900

    Time: 74 ms (51.02%), Space: 14.9 MB (89.85%) - LeetHub

---
+++ 0120-triangle/0120-triangle.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        for r in range(1, len(triangle)):
+            for c in range(len(triangle[r])):
+                if c == 0:
+                    triangle[r][c] += triangle[r - 1][0]
+                    
+                elif c == r:
+                    triangle[r][c] += triangle[r - 1][-1]
+                    
+                else:
+                    triangle[r][c] += min(triangle[r - 1][c - 1], triangle[r - 1][c])
+                    
+        return min(triangle[-1])
