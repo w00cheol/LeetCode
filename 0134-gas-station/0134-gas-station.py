5ce5262f761f5e59125f477850a828e02e8f2b58
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2023-02-21 17:12:35 +0900

    Time: 1103 ms (14.01%), Space: 19.8 MB (26.42%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,12 +1,9 @@
 class Solution:
     def canCompleteCircuit(self, gas, cost):
-        diff = [gas[i] - cost[i] for i in range(len(gas))]
+        answer, require, money = 0, 0, 0
         
-        answer = 0
-        require, money = 0, 0
-        
-        for i in range(len(diff)):
-            money += diff[i]
+        for i in range(len(gas)):
+            money += gas[i] - cost[i]
             
             if money < 0:
                 answer = i + 1
