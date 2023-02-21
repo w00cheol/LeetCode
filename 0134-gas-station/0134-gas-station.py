8b1e91746137a8c627a77938b49d3dca03e91aaf
권우철(Knox Woocheol Kwon) <kw8384@gmail.com>
2023-02-21 17:12:02 +0900

    Time: 1133 ms (10.72%), Space: 19.4 MB (37.50%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,5 +1,5 @@
 class Solution:
-    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+    def canCompleteCircuit(self, gas, cost):
         diff = [gas[i] - cost[i] for i in range(len(gas))]
         
         answer = 0
@@ -13,7 +13,7 @@ class Solution:
                 require += money
                 money = 0
                 
-        if answer < len(gas) and money + require >= 0:
+        if money + require >= 0:
             return answer
         else:
             return -1
